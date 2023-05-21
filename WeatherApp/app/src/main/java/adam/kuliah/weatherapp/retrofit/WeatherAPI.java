27f2579934b0597252df1045049c260e3221110f
adamdaffa56 <89517434+adamdaffa56@users.noreply.github.com>
2023-05-21 21:05:56 +0700

    Commit sourcecode WeatherApp

---
+++ WeatherApp/app/src/main/java/adam/kuliah/weatherapp/retrofit/WeatherAPI.java
@@ -0,0 +1,9 @@
+package adam.kuliah.weatherapp.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface WeatherAPI {
+    @GET("forecast?latitude=-7.98&longitude=112.63&daily=weathercode&current_weather=true&timezone=auto")
+    Call<RecyclerData> getData();
+}
